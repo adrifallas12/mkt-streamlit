63da81ac30c66559351cb1b4b887c1465d794e9b
Adriana Fallas <adriana_fallas@Adriana-Fallas-Hakkoda-X6FD2XP62P.local>
2023-10-10 10:41:42 -0600

    added some st widgets

--- prueba_streamlit.py
+++ prueba_streamlit.py
@@ -1,1 +1,9 @@
-print('hello')
+import pandas as pd
+import streamlit as st
+
+st.title('First Attempt')
+
+st.checkbox('we are cool')
+st.radio('Pick your favorite programming language!', ['Python', 'SQL', 'JavaScript', 'C++', "Perl"])
+st.selectbox('Pick your name', ['Adriana', 'Jonathan', 'Javier', 'Sofia'])
+st.select_slider('How cold is it?', ['Hot', 'Warm', 'Cold', 'Freezing'])
