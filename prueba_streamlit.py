090b486005609b05c791a26dceea84dcf20956ba
Adriana Fallas <adriana_fallas@Adriana-Fallas-Hakkoda-X6FD2XP62P.local>
2023-10-10 11:02:34 -0600

    added file uploader

--- prueba_streamlit.py
+++ prueba_streamlit.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import requests
 
 st.title('First Attempt')
 
@@ -7,3 +8,9 @@ st.checkbox('we are cool')
 st.radio('Pick your favorite programming language!', ['Python', 'SQL', 'JavaScript', 'C++', "Perl"])
 st.selectbox('Pick your name', ['Adriana', 'Jonathan', 'Javier', 'Sofia'])
 st.select_slider('How cold is it?', ['Hot', 'Warm', 'Cold', 'Freezing'])
+
+upload_file = st.file_uploader("Upload your files", type=None, accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
+
+if upload_file is not None:
+    dataframe = pd.read_csv(upload_file)
+    st.write(dataframe)
