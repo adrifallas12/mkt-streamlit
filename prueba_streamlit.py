62e75652614dadc7418a85a300b8a0969c06c5d9
Adriana Fallas <adriana_fallas@Adriana-Fallas-Hakkoda-X6FD2XP62P.local>
2023-10-10 11:11:08 -0600

    tried print tabla

--- prueba_streamlit.py
+++ prueba_streamlit.py
@@ -13,4 +13,5 @@ upload_file = st.file_uploader("Upload your files", type=None, accept_multiple_f
 
 if upload_file is None:
     dataframe = pd.read_csv(upload_file)
-    st.table(dataframe)
+    tabla = st.dataframe(dataframe)
+    print(tabla)
