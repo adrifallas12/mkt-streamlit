b5dea81a5b6710f232754804bd2b8facbf82d89e
Adriana Fallas <adriana_fallas@Adriana-Fallas-Hakkoda-X6FD2XP62P.local>
2023-10-10 11:39:07 -0600

    fixed tab space

--- prueba_streamlit.py
+++ prueba_streamlit.py
@@ -12,5 +12,5 @@ st.select_slider('How cold is it?', ['Hot', 'Warm', 'Cold', 'Freezing'])
 # upload_file = st.file_uploader("Upload your files", type=None, accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
-st.write(df)
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
